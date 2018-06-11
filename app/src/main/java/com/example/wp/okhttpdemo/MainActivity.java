e3c6092996189493a5b8a88e01a3eb225d3df282
wp <258188170@qq.com>
2018-06-11 23:11:21 +0800

    同步,异步

--- app/src/main/java/com/example/wp/okhttpdemo/MainActivity.java
+++ app/src/main/java/com/example/wp/okhttpdemo/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.wp.okhttpdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -28,15 +29,35 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    public void request() {
+    public void reques() {
+        //同步
         OkHttpClient client = new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build();
         Request request = new Request.Builder().url("http://www.baidu.com").get().build();
         Call call = client.newCall(request);
         try {
             Response response = call.execute();
-            Log.i(TAG, "request: " + response.body().string());
+            Log.i(TAG, "reques: " + response.body().string());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void request() {
+        //异步
+        OkHttpClient client = new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build();
+        Request request = new Request.Builder().url("http://www.baidu.com").get().build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //失败
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //成功
+                Log.i(TAG, "onResponse: " + response.body().string());
+            }
+        });
     }
 }
